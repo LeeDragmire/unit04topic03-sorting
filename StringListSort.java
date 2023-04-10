cff3d2ff3dea4eb37482d8bbb9bd9bf9ee70c95d
colton <adamscolton64@gmail.com>
2023-04-10 11:26:44 -0500

    StringSort | Complete

--- StringListSort.java
+++ StringListSort.java
@@ -1,12 +1,38 @@
 import java.util.ArrayList;
 
-public class StringListSearch {
+public class StringListSort {
 
-    public static int unorderedSearch(ArrayList<String> items, String term) {
-        return 0;
+    public static void selectionSort(ArrayList<String> items) {
+        for (int i = 0; i < items.size(); i++) {
+            String minVal = items.get(i);
+            int minPos = i;
+            for (int j = i + 1; j < items.size(); j++) {
+                if (minVal.compareTo(items.get(j)) > 0) {
+                    minVal = items.get(j);
+                    minPos = j;
+                }
+            }
+            if (minPos != i) {
+                String tmp = items.get(i);
+                items.set(i, minVal);
+                items.set(minPos, tmp);
+            }
+        }
+    }
+
+    public static void insertionSort(ArrayList<String> items) {
+        for (int i = 1; i < items.size(); i++) {
+            int j = i;
+            while (j > 0 && items.get(j).compareTo(items.get(j-1)) < 0) {
+                String tmp = items.get(j);
+                items.set(j, items.get(j-1));
+                items.set(j-1, tmp);
+                j--;
+            }
+        }
     }
     
     public static void main(String[] args) {
-        
-    }
+
+    } 
 }
--- StringListSortTest.java
+++ StringListSortTest.java
@@ -8,6 +8,44 @@ import org.junit.Test;
 public class StringListSortTest {
     @Test
     public void testSelectionSort() {
-        
+        ArrayList<String> input;
+        ArrayList<String> expectedOutput;
+        ArrayList<String> actualOutput;
+
+
+        input = new ArrayList<>(Arrays.asList("hello", "to", "farewell", "goodbye", "hi"));
+        expectedOutput = new ArrayList<>(Arrays.asList("farewell", "goodbye", "hello", "hi", "to"));
+        StringListSort.selectionSort(input);
+        actualOutput = input;
+        assertEquals(expectedOutput, actualOutput);
+
+
+        input = new ArrayList<>(Arrays.asList("hello", "to", "farewell", "goodbye", "hi", "a", "and", "there"));
+        expectedOutput = new ArrayList<>(Arrays.asList("a", "and", "farewell", "goodbye", "hello", "hi", "there", "to"));
+        StringListSort.selectionSort(input);
+        actualOutput = input;
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+
+    @Test
+    public void testInsertionSort() {
+        ArrayList<String> input;
+        ArrayList<String> expectedOutput;
+        ArrayList<String> actualOutput;
+
+
+        input = new ArrayList<>(Arrays.asList("hello", "to", "farewell", "goodbye", "hi"));
+        expectedOutput = new ArrayList<>(Arrays.asList("farewell", "goodbye", "hello", "hi", "to"));
+        StringListSort.insertionSort(input);
+        actualOutput = input;
+        assertEquals(expectedOutput, actualOutput);
+
+
+        input = new ArrayList<>(Arrays.asList("to", "hi", "farewell", "goodbye", "hello"));
+        expectedOutput = new ArrayList<>(Arrays.asList("farewell", "goodbye", "hello", "hi", "to"));
+        StringListSort.insertionSort(input);
+        actualOutput = input;
+        assertEquals(expectedOutput, actualOutput);
     }
 }
